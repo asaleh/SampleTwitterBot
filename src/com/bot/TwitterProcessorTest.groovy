1b21364546ffba11e1a98b68301fc3995130806c
Ali Saleh <ali.saleh@metrigo.de>
2016-05-04 17:24:20 +0200

    Develop branch created. unit test added

--- src/com/bot/TwitterProcessorTest.groovy
+++
@@ -1,7 +0,0 @@
-package com.bot
-
-/**
- * Created by alisaleh on 04.05.16.
- */
-class TwitterProcessorTest extends groovy.util.GroovyTestCase {
-}
---
+++ src/com/bot/test/TwitterProcessorTest.groovy
@@ -0,0 +1,27 @@
+package com.bot.test
+
+/**
+ * Created by alisaleh on 04.05.16.
+ */
+class TwitterProcessorTest extends GroovyTestCase {
+    void setUp() {
+        super.setUp()
+
+    }
+
+    void testPostStaticTweet() {
+
+    }
+
+    void testPostDynamicallyGeneratedTweet() {
+
+    }
+
+    void testReplyToTopNTweetsForSpecificKeyword() {
+
+    }
+
+    void testGetTopNTweetsForKeyword() {
+
+    }
+}
